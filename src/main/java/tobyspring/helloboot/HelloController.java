7f784a77e19327766d5d4426e908da42a7e94722
kw.choi <kw.choi@fastfive.co.kr>
2024-07-02 15:40:29 +0900

    애노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,14 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
     // 멤버 변수
     // 한번 할당 후 재정의는 없다
@@ -11,6 +18,9 @@ public class HelloController {
         this.simpleHelloService = simpleHelloService;
     }
 
+    @GetMapping
+    // String으로 반환할 경우 View로 인식하기 때문에 ResponseBody 추가
+    @ResponseBody
     public String hello(String name) {
         // null 체크
         return simpleHelloService.sayHello(Objects.requireNonNull(name));
