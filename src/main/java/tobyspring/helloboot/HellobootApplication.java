832d41c3d00b8b8f2b33acdae1aadb36a2524410
kw.choi <kw.choi@fastfive.co.kr>
2024-07-02 16:10:09 +0900

    java code 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,37 +2,35 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration // 스프링 컨테이너에게 이 클래스가 설정 정보를 포함하고 있음을 알려줌
+// 팩토리 메소드가 있는 것을 알려줌
 public class HellobootApplication {
+    // bean annotation을 붙이면 클래
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
-        // 서블릿 컨테이너 등록
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         // Jetty나 undertow 같은 서블릿 컨테이너 사용 가능
         //        new JettyServletWebServerFactory()
         //        new UndertowServletWebServer()
-
-
-//        WebServer webServer = factory.getWebServer(servletContext -> {
-//            // 서블릿 등록
-//            // 익명 클래스 등록
-//            servletContext.addServlet("hello", new HttpServlet() {
-//                @Override
-//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//                    resp.setStatus(HttpStatus.OK.value());
-//                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-//                    PrintWriter writer = resp.getWriter();
-//                    writer.println("hello servlet!!");
-//                }
-//            }).addMapping("/hello");
-//        });
-
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        // GenericWebApplicationContext은 configure를 읽을수 없음
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
+                // 서블릿 컨테이너 등록
+                TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
                 // 스프링 컨테이너로의 이전
                 WebServer webServer = factory.getWebServer(servletContext -> {
                     // 서블릿 등록
@@ -48,8 +46,8 @@ public class HellobootApplication {
             }
 
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        // bean object 등록
+        applicationContext.register(HellobootApplication.class);
         applicationContext.refresh();
 
         // 서블릿 등록
