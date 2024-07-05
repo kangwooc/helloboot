2ecbea2e0c44be02a5d96a8f3b22e886b6109206
kw.choi <kw.choi@fastfive.co.kr>
2024-07-05 16:32:46 +0900

    test code

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,27 @@
+package tobyspring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                testRestTemplate.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header 중 content-type: text/plain;charset=UTF-8
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body "Hello Spring"
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
