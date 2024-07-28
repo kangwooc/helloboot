4470e213bb554ade89fef398ead29a26f27b54d4
Kangwoo choi <kangwooc@gmail.com>
2024-07-28 15:17:48 +0900

    합성 애노테이션

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,27 +1,7 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
-
-@Configuration
-@ComponentScan // 하위 패키지를 전부 스캔
+@MySpringBootAnnotation
 public class HellobootApplication {
-    // 이거는 사용해야하는데 어디서 구현을?????
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-    // DispatcherServlet는 ApplicationContextAware를 구현을 하는데
-    // ApplicationContextAware는 컨테이너가 관리하는 빈을 주입받을 수 있다
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
     public static void main(String[] args) {
         MySpringApplication.run(HellobootApplication.class, args);
     }
---
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,19 @@
+package tobyspring.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// default는 class이다
+// loading할때는 사라지기 때문에
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration
+@ComponentScan
+public @interface MySpringBootAnnotation {
+
+}
