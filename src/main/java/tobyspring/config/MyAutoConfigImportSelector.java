1b5ed60c757b6c58a62fbe07558c546a19836843
Kangwoo choi <kangwooc@gmail.com>
2024-08-03 19:40:38 +0900

    auto config 파일 생성

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,31 @@
 package tobyspring.config;
 
-import org.springframework.context.annotation.ImportSelector;
+import org.springframework.boot.context.annotation.ImportCandidates;
+import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MyAutoConfigImportSelector implements ImportSelector {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-            "tobyspring.config.autoconfig.DispatcherServletConfig",
-            "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+//        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+//
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+        List<String> autoConfigs = new ArrayList<>();
+//        for (String candidate: ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+//            autoConfigs.add(candidate);
+//        }
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+//        autoConfigs.stream()
+        // Arrays.copyOf
+        return autoConfigs.toArray(new String[0]);
     }
 }
